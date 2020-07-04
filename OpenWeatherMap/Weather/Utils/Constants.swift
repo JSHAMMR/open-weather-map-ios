4e52a5081a375f41c5f83909115a0d06f074d164
Jamil Shamar <jameelshammr@gmail.com>
2020-07-04 23:22:32 +0800

    add constant values

---
+++ OpenWeatherMap/Weather/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  OpenWeatherMap
+//
+//  Created by Gamil Ali Qaid Shamar on 04/07/2020.
+//  Copyright © 2020 Jamil. All rights reserved.
+//
+
+import Foundation
+public let openWeatherMapLink =  "http://api.openweathermap.org/data/2.5/weathe"
+public let API_Key = "1da6712886d3a48b9ff4fe48895916c6"
